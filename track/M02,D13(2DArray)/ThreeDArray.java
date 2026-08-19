28cb630c013cbab397700b26c80f6b98da28b46f
Akshata Karajagi <karajagiakshata@gmail.com>
2026-08-19 13:22:47 +0530

    3d1

--- track/M02,D13(2DArray)/ThreeDArray.java
+++ track/M02,D13(2DArray)/ThreeDArray.java
@@ -7,17 +7,17 @@ class ThreeDArray {
         int a[][][] = new int[3][3][5];
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter the Array Elements: ");
-        for (int i = 0; i <= 2; i++) {
-            for (int j = 0; j <= 4; j++) {
-                for (int k = 0; k <= 4; k++) {
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[i].length; j++) {
+                for (int k = 0; k < a[i][j].length; k++) {
                     a[i][j][k] = scan.nextInt();
                 }
             }
         }
         System.out.println("Array Elements are : ");
-        for (int i = 0; i <= 2; i++) {
-            for (int j = 0; j <= 3; j++) {
-                for (int k = 0; k <= 4; k++) {
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[i].length; j++) {
+                for (int k = 0; k < a[i][j].length; k++) {
                     System.out.print(a[i][j][k] + " ");
                 }
                 System.out.println();
